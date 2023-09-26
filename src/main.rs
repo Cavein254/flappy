da59a4a611278bfa526878043f97f22c6dd7a7c3
John Muthua <caveomuthama@gmail.com>
2023-09-26 21:49:04 +0300

    Add clippy

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,57 @@
 use bracket_lib::prelude::*;
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
 enum GameMode {
     Menu,
     Playing,
     End,
 }
 
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437("@"));
+    }
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
@@ -21,6 +60,8 @@ impl State {
         self.mode = GameMode::End;
     }
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
     fn main_menu(&mut self, ctx: &mut BTerm) {
